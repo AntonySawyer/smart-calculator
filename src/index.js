6f4f4b2f6c812308eda16c61dbf152904d574105
Antony <A.Krutenok@gmail.com>
2018-10-03 12:33:42 +0300

    add feature

--- src/index.js
+++ src/index.js
@@ -1,26 +1,100 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.equals = [initialValue];
+    this.fullExpression = [initialValue];
+}
+//While for right order. Switch for calculation.
+  order(operatorIndex) {
+    let first = this.equals[operatorIndex - 1];
+    let second = this.equals[operatorIndex + 1];
+    switch (this.equals[operatorIndex]) {
+      case '+':
+        this.changeEquals(operatorIndex - 1, first + second);
+      break;
+      case '-':
+        this.changeEquals(operatorIndex - 1, first - second);
+      break;
+      case '*':
+        this.changeEquals(operatorIndex - 1, first * second);
+      break;
+      case '/':
+        this.changeEquals(operatorIndex - 1, first / second);
+      break;
+      case '^':
+        this.changeEquals(operatorIndex - 1, Math.pow(first, second));
+      break;
+    }
+    while (this.equals.length > 1) {
+      if (this.equals.includes('^')) {
+        this.order(this.equals.lastIndexOf('^'));
+        break;
+      } else if (this.equals.includes('*')) {
+        this.order(this.equals.indexOf('*'));
+        break;
+    } else if (this.equals.includes('/')) {
+        this.order(this.equals.indexOf('/'));
+        break;
+    } else if (this.equals.includes('+') || this.equals.includes('-')) {
+      let add = this.equals.indexOf('+');
+      let substract = this.equals.indexOf('-');
+      if (substract != -1 && add != -1) {
+        this.order(Math.min(substract, add));
+      } else if (substract != -1) {
+        this.order(substract);
+      } else {
+        this.order(add);
+      }
+        break;
+    }
+  }
+  this.valueOf = function() {return this.equals[0].toString();}
+  return this.valueOf();
+}
+// Most used expression in functions:
+// changeEquals - for change result of calculation
+  changeEquals (start, value) {
+    this.equals.splice(start, 3, value);
+  }
+  // recoveryEquals - for change temporary array into full expression
+  recoveryEquals () {
+    if (this.fullExpression.length > 1) {
+      this.equals = this.fullExpression.concat();
+    }
+  }
+// next: just add and recalculate
+  add(number) {
+    this.fullExpression.push('+', number);
+    this.recoveryEquals();
+    this.order('');
+    return this;
   }
 
-  add(number) {
-    // your implementation
-  }
-  
   subtract(number) {
-    // your implementation
+    this.fullExpression.push('-', number);
+    this.recoveryEquals();
+    this.order('');
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.fullExpression.push('*', number);
+    this.recoveryEquals();
+    this.order('');
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.fullExpression.push('/', number);
+    this.recoveryEquals();
+    this.order('');
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.fullExpression.push('^', number);
+    this.recoveryEquals();
+    this.order('');
+    return this;
   }
 }
 
